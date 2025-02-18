18e44cdd500f9e93799de308377c64e4a5071d4b
Aquatic72 <akri2022v@gmail.com>
2025-02-19 03:35:40 +0500

    fixed error in my_file.py

--- my_file.py
+++ my_file.py
@@ -1,3 +1,4 @@
 print("Here's my file for your git repository")
-a, b  =  int(input("Enter 2 numbers using SPACE"))
+a, b  =  [int(number) for number in input("Enter 2 numbers using SPACE: ").split()]
 print(f"The sum of your numbers is {a + b}")
+input("Press any key to exit")
